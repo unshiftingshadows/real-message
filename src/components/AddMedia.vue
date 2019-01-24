e7467751a652afa277fa64938aa1cab451d51d00
Jacob Beck <jacob@unshiftingshadows.com>
2019-01-24 12:21:08 -0600

    Removed option to upload image
    Only image links will be accepted to save on space

--- src/components/AddMedia.vue
+++ src/components/AddMedia.vue
@@ -21,17 +21,17 @@
         <q-input type="text" float-label="Lyric Title" v-model="lyricTitle" />
       </div>
       <div class="col-12" v-if="type === 'image'">
-        <q-btn-toggle
+        <!-- <q-btn-toggle
           v-model="imageType"
           toggle-color="primary"
           :options="imageOptions"
         />
         <br/>
-        <br/>
-        <div class="col-12" v-if="imageType === 'wiki'">
+        <br/> -->
+        <!-- <div class="col-12" v-if="imageType === 'wiki'">
           <q-input type="text" float-label="Wiki Title" v-model="imageTitle" />
-        </div>
-        <div class="col-12" v-if="imageType === 'upload'">
+        </div> -->
+        <!-- <div class="col-12" v-if="imageType === 'upload'">
           <FilePond
             name="image-upload"
             ref="image-upload"
@@ -41,18 +41,18 @@
             :files="images"
             :server="imageServer"
           />
-        </div>
-        <div class="col-12" v-if="imageType === 'link'">
+        </div> -->
+        <div class="col-12">
           <q-input type="text" float-label="Image URL" v-model="imageURL" />
         </div>
       </div>
       <div class="col-12" v-if="type === 'video'">
         <q-input type="text" float-label="Video URL" v-model="videoURL" />
       </div>
-      <div class="col-12" v-if="!(type === 'image' && imageType === 'upload')">
+      <!-- <div class="col-12" v-if="!(type === 'image' && imageType === 'upload')">
         <q-btn color="primary" class="float-right" @click.native="add" :disabled="loading">Add {{ type }}</q-btn>
         <q-spinner class="float-right" size="2rem" color="primary" style="margin-right: 20px;" v-if="loading" />
-      </div>
+      </div> -->
     </div>
   </q-modal>
 </template>
@@ -97,73 +97,73 @@ export default {
       illustrationTitle: '',
       lyricTitle: '',
       videoURL: '',
-      imageType: '',
+      // imageType: '',
       imageTitle: '',
-      images: [],
-      imageURL: '',
-      imageOptions: [
-        // {
-        //   label: 'Wiki',
-        //   value: 'wiki'
-        // },
-        {
-          label: 'Upload',
-          value: 'upload'
-        },
-        {
-          label: 'Link',
-          value: 'link'
-        }
-      ],
-      imageServer: {
-        process: (fieldName, file, metadata, load, error, progress, abort) => {
-          // Should do custom file upload or local storing here
-          this.loading = true
-          this.$firebase.list(this.type).add({ service: 'upload', user: this.$firebase.auth.currentUser.uid }).then((res) => {
-          // this.$database.add('image', { service: 'upload' }, (res) => {
-            // GA - Add image media event
-            this.$ga.event('media', 'add', 'image-upload')
-            var uploadProcess = this.$firebase.imagesRef.child(res.id).put(file)
-            uploadProcess.on('state_changed', (snapshot) => {
-              progress(true, snapshot.bytesTransferred, snapshot.totalBytes)
-            }, (err) => {
-              error(err)
-            }, () => {
-              load(uploadProcess.snapshot.ref.name)
-              uploadProcess.snapshot.ref.getDownloadURL().then((url) => {
-                this.refresh(this.type)
-                this.showAddMedia = false
-                this.init()
-              })
-            })
-          })
+      // images: [],
+      // imageURL: '',
+      // imageOptions: [
+      //   // {
+      //   //   label: 'Wiki',
+      //   //   value: 'wiki'
+      //   // },
+      //   // {
+      //   //   label: 'Upload',
+      //   //   value: 'upload'
+      //   // },
+      //   {
+      //     label: 'Link',
+      //     value: 'link'
+      //   }
+      // ],
+      // imageServer: {
+      //   process: (fieldName, file, metadata, load, error, progress, abort) => {
+      //     // Should do custom file upload or local storing here
+      //     this.loading = true
+      //     this.$firebase.list(this.type).add({ service: 'upload', user: this.$firebase.auth.currentUser.uid }).then((res) => {
+      //     // this.$database.add('image', { service: 'upload' }, (res) => {
+      //       // GA - Add image media event
+      //       this.$ga.event('media', 'add', 'image-upload')
+      //       var uploadProcess = this.$firebase.imagesRef.child(res.id).put(file)
+      //       uploadProcess.on('state_changed', (snapshot) => {
+      //         progress(true, snapshot.bytesTransferred, snapshot.totalBytes)
+      //       }, (err) => {
+      //         error(err)
+      //       }, () => {
+      //         load(uploadProcess.snapshot.ref.name)
+      //         uploadProcess.snapshot.ref.getDownloadURL().then((url) => {
+      //           this.refresh(this.type)
+      //           this.showAddMedia = false
+      //           this.init()
+      //         })
+      //       })
+      //     })
 
-          // Can call the error method if something is wrong, should exit after
-          // error('oh my goodness');
+      //     // Can call the error method if something is wrong, should exit after
+      //     // error('oh my goodness');
 
-          // Should call the progress method to update the progress to 100% before calling load
-          // (endlessMode, processedSize, totalSize)
-          // progress(true, 0, 1024);
+      //     // Should call the progress method to update the progress to 100% before calling load
+      //     // (endlessMode, processedSize, totalSize)
+      //     // progress(true, 0, 1024);
 
-          // Should call the load method when done and pass the returned server file id
-          // the unique server file id is used by revert and restore functions
-          // load('unique-file-id');
+      //     // Should call the load method when done and pass the returned server file id
+      //     // the unique server file id is used by revert and restore functions
+      //     // load('unique-file-id');
 
-          // Should expose an abort method so the request can be cancelled
-          return {
-            abort: () => {
-              // User tapped abort, cancel our ongoing actions here
+      //     // Should expose an abort method so the request can be cancelled
+      //     return {
+      //       abort: () => {
+      //         // User tapped abort, cancel our ongoing actions here
 
-              // Let FilePond know the request has been cancelled
-              abort()
-            }
-          }
-        },
-        revert: './revert',
-        restore: './restore/',
-        load: './load/',
-        fetch: './fetch/'
-      },
+      //         // Let FilePond know the request has been cancelled
+      //         abort()
+      //       }
+      //     }
+      //   },
+      //   revert: './revert',
+      //   restore: './restore/',
+      //   load: './load/',
+      //   fetch: './fetch/'
+      // },
       types: ['quote', 'image', 'illustration', 'lyric', 'video'],
       showAddMedia: false
     }
@@ -200,29 +200,29 @@ export default {
             obj.title = this.quoteTitle
             break
           case 'image':
-            switch (this.imageType) {
-              case 'wiki':
-                if (this.imageTitle === '') {
-                  this.$q.notify('Please review fields again')
-                  return
-                }
-                obj.title = this.imageTitle
-                obj.service = 'wiki'
-                break
-              case 'upload':
-                return
-              case 'link':
-                if (this.imageURL === '') {
-                  this.$q.notify('Please review fields again')
-                  return
-                }
-                obj.imageURL = this.imageURL
-                obj.service = 'link'
-                break
-              default:
-                console.error('invalid image type')
+            // switch (this.imageType) {
+            //   case 'wiki':
+            //     if (this.imageTitle === '') {
+            //       this.$q.notify('Please review fields again')
+            //       return
+            //     }
+            //     obj.title = this.imageTitle
+            //     obj.service = 'wiki'
+            //     break
+            //   case 'upload':
+            //     return
+            //   case 'link':
+            if (this.imageURL === '') {
+              this.$q.notify('Please review fields again')
+              return
             }
+            obj.imageURL = this.imageURL
+            obj.service = 'link'
             break
+            //   default:
+            //     console.error('invalid image type')
+            // }
+            // break
           case 'illustration':
             if (this.illustrationTitle === '') {
               this.$q.notify('Please review fields again')
