fed432dbadc2caa72132224c3424823c7487fac0
Jacob Beck <jacob@unshiftingshadows.com>
2019-02-25 13:11:12 -0600

    Suspended sentry

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -4,41 +4,45 @@ let uEmail = ''
 let uName = ''
 
 function setUser (uid, email, username) {
-  uEmail = email
-  uName = username
-  Sentry.configureScope((scope) => {
-    scope.setUser({
-      email: email,
-      id: uid,
-      username: username
+  if (!process.env.DEV) {
+    uEmail = email
+    uName = username
+    Sentry.configureScope((scope) => {
+      scope.setUser({
+        email: email,
+        id: uid,
+        username: username
+      })
     })
-  })
+  }
 }
 
 // leave the export, even if you don't use it
 export default ({ app, router, Vue }) => {
-  Sentry.init({
-    dsn: 'https://84905595bcfa45f29fc68ec5886b8754@sentry.io/1372280',
-    integrations: [new Sentry.Integrations.Vue({ Vue })],
-    release: `real-message@${process.env.VER}`,
-    environment: process.env.DEV ? 'staging' : 'prod',
-    beforeSend (event) {
-      console.error('event error', event.exception.values[0].value)
-      if (event.exception.values[0].value === 'ResizeObserver loop limit exceeded') {
-        console.info('ResizeObserver loop limit exceeded')
-        return null
+  if (!process.env.DEV) {
+    Sentry.init({
+      dsn: 'https://84905595bcfa45f29fc68ec5886b8754@sentry.io/1372280',
+      integrations: [new Sentry.Integrations.Vue({ Vue })],
+      release: `real-message@${process.env.VER}`,
+      environment: process.env.DEV ? 'staging' : 'prod',
+      beforeSend (event) {
+        console.error('event error', event.exception.values[0].value)
+        if (event.exception.values[0].value === 'ResizeObserver loop limit exceeded') {
+          console.info('ResizeObserver loop limit exceeded')
+          return null
+        }
+        if (event.exception) {
+          Sentry.showReportDialog({
+            user: {
+              email: uEmail,
+              name: uName
+            }
+          })
+        }
+        return event
       }
-      if (event.exception) {
-        Sentry.showReportDialog({
-          user: {
-            email: uEmail,
-            name: uName
-          }
-        })
-      }
-      return event
-    }
-  })
+    })
+  }
   Vue.prototype.$sentry = {
     setUser,
     err: Sentry.captureException,
