81c73f3a5634f7b54f3d36fc9782435ba3675506
Jacob Beck <jacob@unshiftingshadows.com>
2019-02-28 17:32:32 -0600

    Update sentry.js

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -19,30 +19,28 @@ function setUser (uid, email, username) {
 
 // leave the export, even if you don't use it
 export default ({ app, router, Vue }) => {
-  if (!process.env.DEV) {
-    Sentry.init({
-      dsn: 'https://84905595bcfa45f29fc68ec5886b8754@sentry.io/1372280',
-      integrations: [new Sentry.Integrations.Vue({ Vue })],
-      release: `real-message@${process.env.VER}`,
-      environment: process.env.DEV ? 'staging' : 'prod',
-      beforeSend (event) {
-        console.error('event error', event.exception.values[0].value)
-        if (event.exception.values[0].value === 'ResizeObserver loop limit exceeded') {
-          console.info('ResizeObserver loop limit exceeded')
-          return null
-        }
-        if (event.exception) {
-          Sentry.showReportDialog({
-            user: {
-              email: uEmail,
-              name: uName
-            }
-          })
-        }
-        return event
+  Sentry.init({
+    dsn: 'https://84905595bcfa45f29fc68ec5886b8754@sentry.io/1372280',
+    integrations: [new Sentry.Integrations.Vue({ Vue })],
+    release: `real-message@${process.env.VER}`,
+    environment: process.env.DEV ? 'staging' : 'prod',
+    beforeSend (event) {
+      console.error('event error', event.exception.values[0].value)
+      if (event.exception.values[0].value === 'ResizeObserver loop limit exceeded') {
+        console.info('ResizeObserver loop limit exceeded')
+        return null
       }
-    })
-  }
+      if (event.exception) {
+        Sentry.showReportDialog({
+          user: {
+            email: uEmail,
+            name: uName
+          }
+        })
+      }
+      return event
+    }
+  })
   Vue.prototype.$sentry = {
     setUser,
     err: Sentry.captureException,
