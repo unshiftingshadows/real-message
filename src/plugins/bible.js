e77511d672d636b838a49df37627a1efb36784aa
Jacob Beck <jacob@unshiftingshadows.com>
2019-05-09 14:53:42 -0500

    Bible chapters
    Readable refs now display full chapters better

--- src/plugins/bible.js
+++ src/plugins/bible.js
@@ -14,16 +14,24 @@ export function readable (ref) {
   // var text = bcv.osis().replace(/(\d+).(\d+)/g, '$1:$2')
   // console.log(text)
   // console.log('bcv', bcv)
-  console.log('run readable')
+  console.log('run readable', bcv)
   if (bcv.entities[0].passages[0].start.b === bcv.entities[0].passages[0].end.b) {
     if (bcv.entities[0].passages[0].start.c === bcv.entities[0].passages[0].end.c) {
-      if (bcv.entities[0].passages[0].start.v === bcv.entities[0].passages[0].end.v) {
-        return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v
+      if (bcv.entities[0].passages[0].start.v) {
+        if (bcv.entities[0].passages[0].start.v === bcv.entities[0].passages[0].end.v) {
+          return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v
+        } else {
+          return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v + '-' + bcv.entities[0].passages[0].end.v
+        }
       } else {
-        return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v + '-' + bcv.entities[0].passages[0].end.v
+        return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c
       }
     } else {
-      return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v + '-' + bcv.entities[0].passages[0].end.c + ':' + bcv.entities[0].passages[0].end.v
+      if (bcv.entities[0].passages[0].start.v) {
+        return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + ':' + bcv.entities[0].passages[0].start.v + '-' + bcv.entities[0].passages[0].end.c + ':' + bcv.entities[0].passages[0].end.v
+      } else {
+        return bcv.entities[0].passages[0].start.b + ' ' + bcv.entities[0].passages[0].start.c + '-' + bcv.entities[0].passages[0].end.c
+      }
     }
   } else {
     console.error('Books dont match...')
