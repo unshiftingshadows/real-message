47e490dd6ac149a8fd1e4c095998f289ce21dbfd
Jacob Beck <jacob@unshiftingshadows.com>
2019-09-06 15:41:15 -0500

    Update Content.vue
    Basics for content slide modal

--- src/components/slide/Content.vue
+++ src/components/slide/Content.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>My component</div>
+  <q-modal ref="modal" content-classes="slide-modal" maximized>
+    <q-btn
+      color="primary"
+      @click.native="$refs.modal.hide()"
+      icon="fas fa-times"
+      class="float-right"
+      size="sm"
+    />
+    To build a content slide...
+  </q-modal>
 </template>
 
 <script>
 export default {
   name: 'slide-content',
+  props: ['data', 'id'],
   data () {
     return {}
+  },
+  methods: {
+    open () {
+      this.$refs.modal.show()
+    },
+    close () {
+      this.$refs.modal.hide()
+    }
   }
 }
 </script>
